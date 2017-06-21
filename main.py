7dbd9f0c47835f3831231be6fd84f6f735e8326f
Obtuse <theobtuseautodidact@gmail.com>
2017-06-21 12:19:20 -0700

    Add time_form.html template and refactor /validate-time to use it

--- main.py
+++ main.py
@@ -1,54 +1,63 @@
 from flask import Flask, request, redirect
 import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-<html>
-    <body>
-        <form action="/hello" method="POST">
-            <label for="first_name">First Name</label>
-            <input id="first_name" type="text" name="first_name">
-            <input type="submit">
-        </form>
-    </body>
-</html>
-"""
+# form = """
+# <!doctype html>
+# <html>
+#     <body>
+#         <form action="/hello" method="POST">
+#             <label for="first_name">First Name</label>
+#             <input id="first_name" type="text" name="first_name">
+#             <input type="submit">
+#         </form>
+#     </body>
+# </html>
+# """
 
-time_form = """
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>Validation Time</h1>
-    <form method="POST">
-        <label>
-            Hours (24-hour format)
-            <input name="hours" type="text" value="{hours}">
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>
-            Minutes
-            <input name="minutes" type="text" value="{minutes}">
-        </label>
-        <p class="error">{minutes_error}</p>
-        <input type="submit" value="Validate">
-    </form>
-"""
+# time_form = """
+#     <style>
+#         .error {{ color: red; }}
+#     </style>
+#     <h1>Validation Time</h1>
+#     <form method="POST">
+#         <label>
+#             Hours (24-hour format)
+#             <input name="hours" type="text" value="{hours}">
+#         </label>
+#         <p class="error">{hours_error}</p>
+#         <label>
+#             Minutes
+#             <input name="minutes" type="text" value="{minutes}">
+#         </label>
+#         <p class="error">{minutes_error}</p>
+#         <input type="submit" value="Validate">
+#     </form>
+# """
 
 @app.route("/")
 def index():
-    return form
+    template = jinja_env.get_template("hello_form.html")
+    return template.render()
 
 @app.route("/hello", methods=['POST'])
 def hello():
     first_name = request.form["first_name"]
-    return "<h1>Hello, " + cgi.escape(first_name) + "!</h1>"
+    template = jinja_env.get_template("hello_greeting.html")
+    return template.render(first_name=first_name)
+    # return "<h1>Hello, " + cgi.escape(first_name) + "!</h1>"
 
 @app.route("/validate-time")
 def display_time_form():
-    return time_form.format(hours="", hours_error="", minutes="", minutes_error="")
+    template = jinja_env.get_template("time_form.html")
+    return template.render()
 
 
 def is_integer(num):
@@ -91,7 +100,9 @@ def validate_time():
         # return "Success!"
         return redirect("/valid-time?time={0}".format(time))
     else:
-        return time_form.format(hours_error=hours_error, minutes_error=minutes_error, hours=hours, minutes=minutes)
+        template = jinja_env.get_template("time_form.html")
+
+        return template.render(hours_error=hours_error, minutes_error=minutes_error, hours=hours, minutes=minutes)
 
 
 @app.route("/valid-time")
